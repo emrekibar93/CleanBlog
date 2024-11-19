cb4456549e1fb5efbcf4ed2b7bfe43abcab363bc
emrekibar93 <emrekibar93@gmail.com>
2024-11-19 17:23:47 +0300

    Middleware and Template Engine

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require("express")
+const path = require("path")
+const ejs = require("ejs")
+
+//TEMPLATE ENGINE
 const app = express()
+app.set("view engine","ejs");
+
+//MIDDLEWARE
+app.use(express.static("public"));
 
 app.get('/',(req,res)=>{
+    res.render('index')
+})
 
-    const blog = { id: 1, title: "Blog title", description: "Blog description" }
-    res.send(blog);
+app.get('/about',(req,res)=>{
+    res.render('about')
+})
+
+app.get('/add_post',(req,res)=>{
+    res.render('add_post')
+})
+
+app.get('/post',(req,res)=>{
+    res.render('post')
 })
 
 const port=3000;
