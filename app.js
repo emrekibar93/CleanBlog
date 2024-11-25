baf478b631124f5a3f0246242803efb846dd0663
emrekibar93 <emrekibar93@gmail.com>
2024-11-25 18:40:43 +0300

    model ve dinamik icerik eklendi

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require("express")
+const mongoose = require('mongoose');
 const path = require("path")
 const ejs = require("ejs")
+const Post = require('./models/Post')
+
 
 //TEMPLATE ENGINE
 const app = express()
+
 app.set("view engine","ejs");
+
+// connect db
+mongoose.connect('mongodb://localhost/cleanblog-test-db');
 
 //MIDDLEWARE
 app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
-app.get('/',(req,res)=>{
-    res.render('index')
+app.get ('/',async(req,res)=>{
+    const posts = await Post.find({})
+    res.render('index',{posts})
 })
 
 app.get('/about',(req,res)=>{
@@ -23,6 +33,11 @@ app.get('/add_post',(req,res)=>{
 
 app.get('/post',(req,res)=>{
     res.render('post')
+})
+
+app.post('/posts',async (req,res) => {
+    await Post.create(req.body)
+    res.redirect('/')
 })
 
 const port=3000;
